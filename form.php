b0cd523a19568b8be4ee419d04b49ed621626a31
Ratmir1996 <Beson.ru@bk.ru>
2017-12-15 22:26:46 +0300

    sa

--- form.php
+++ form.php
@@ -9,3 +9,4 @@ $massege = "Имя: $name\nТелефон: $phone";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $massege, "Content-type: text/plain; charset= \"utf-8\"\n From: $recepient" );
+?>
