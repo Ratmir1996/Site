5062345ff1b3db2010c2815698cdec50523f978d
Ratmir1996 <Beson.ru@bk.ru>
2017-12-15 22:10:42 +0300

    sda

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$("#form").submit(function() {
 		$.ajax({
-			type: "GET",
+			type: "POST",
 			url: "mail.php",
 			data: $(this).serialize()
 		}).done(function() {
