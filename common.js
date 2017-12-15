887dd037d49e86dea67c093bf739c1b1d9d93e32
Ratmir1996 <Beson.ru@bk.ru>
2017-12-15 22:07:18 +0300

    comfg

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$("#form").submit(function() {
 		$.ajax({
-			type: "POST",
+			type: "GET",
 			url: "mail.php",
 			data: $(this).serialize()
 		}).done(function() {
