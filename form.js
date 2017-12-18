0b3af40f605f7672cc2566811b95a745964958de
Ratmir1996 <Beson.ru@bk.ru>
2017-12-18 23:50:03 +0300

    site

--- form.js
+++
@@ -1,7 +0,0 @@
-var dialog = document.querySelector('dialog');
-   document.querySelector('#order_button').onclick = function() {
-    dialog.showModal();
-   }
-   document.querySelector('#clodeDialog').onclick = function() {
-    dialog.close();
-   }
